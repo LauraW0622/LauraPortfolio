29401dcff8d5cda19d671f7dc7c5cc285cc34e6b
LauraW0622 <ljw5533@psu.edu>
2024-03-21 06:37:53 -0400

    changed home to index

--- Portfolio/LauraWilson/views.py
+++ Portfolio/LauraWilson/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request): 
-    return render(request, 'home.html')
+    return render(request, 'index.html')
 
 def gallery(request): 
     return render(request, 'gallery.html')
